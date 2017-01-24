4cfacedbd4f7bd7e60d13c92be84aee8d019c492
antoine drabble <antoine.drabble@gmail.com>
2017-01-25 00:44:59 +0100

    Put back the passport secret

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'secret':  process.env.PASSPORT_SECRET,
+  'secret':  process.env.PASSPORT_SECRET || 'ofiJwwm9fKWmf9WjfkwmfKW',
   'database': process.env.MONGODB_URI || 'mongodb://localhost/poll'
 };
