0f1bf71c3b60ce6dfdd25301a984dff43ad98ba6
antoine drabble <antoine.drabble@gmail.com>
2017-01-25 00:09:26 +0100

    Remove passport secret

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'secret':  process.env.PASSPORT_SECRET || '/)=6wv789t6f890Gjjv805(t5()',
+  'secret':  process.env.PASSPORT_SECRET,
   'database': process.env.MONGODB_URI || 'mongodb://localhost/poll'
 };
